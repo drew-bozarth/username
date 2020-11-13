5b04b2384219493f41aec8f304a440f223423de1
Drew Bozarth <dbozarth@chapman.edu>
2020-11-13 10:55:31 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Drew Bozarth 
 echo "Enter a Username: "
 read NAME 
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{3-12}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2-11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - must start with a letter!"
 	echo "You must enter a valid username - at least 3 characters, no more than 12!"
