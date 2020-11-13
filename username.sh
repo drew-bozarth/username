50beb14d8f15c7a90b3d0570de8c98ac3233abf8
Drew Bozarth <dbozarth@chapman.edu>
2020-11-13 10:45:46 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -5,8 +5,9 @@ echo "Enter a Username: "
 read NAME 
 while echo "$NAME" | grep -E -v "^[a-z][0-9]{3-12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - must start with a letter!"
+	echo "You must enter a valid username - at least 3 characters, no more than 12!"
+	echo "Enter a username: "
 	read NAME 
 done
 echo "Thank you"
