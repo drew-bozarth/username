78f8ead7f4fc63bf043c23aa76f6d475778023ca
Drew Bozarth <dbozarth@chapman.edu>
2020-11-19 21:32:26 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -6,7 +6,7 @@ read NAME
 while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2-11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - must start with a letter!"
-	echo "You must enter a valid username - at least 3 characters, no more than 12!"
+	echo "You must enter a valid username - at least 3 characters, no more than 12! You can also use an underscore!"
 	echo "Enter a username: "
 	read NAME 
 done
